dc5d5989a10a690f1f0258b1194f01cf821d2253
ayoung134 <ayou134@lsu.edu>
2026-03-24 10:30:37 -0500

    tried to add svgs

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 // JS scripts placed here
+
+// Random generator
+
+// Source - https://stackoverflow.com/a/4550514
+// Posted by Jacob Relkin, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-03-23, License - CC BY-SA 4.0
+
+const cards = ["death", "empress", "fool", "highpriestess", "justice", "lovers",];
+
+const random = Math.floor(Math.random() * cards.length);
+console.log(random, cards[random]);
